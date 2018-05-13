6bd4a84b47762284acc80a0923434cd1859bffc4
Roberto Nogueira <enogrob@gmail.com>
2018-05-13 20:14:31 -0300

    add-obscuring_references-class

--- spec/pood_spec.rb
+++ spec/pood_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../gear'
+require_relative '../obscuring_references'
 
 RSpec.describe 'POOD App' do
 
@@ -26,6 +27,24 @@ RSpec.describe 'POOD App' do
       expect(Gear.new(52,11, 26, 1.5).gear_inches.round(2)).to be(137.09)
       expect(Gear.new(52,11, 24, 1.25).gear_inches.round(2)).to be(125.27)
     end
+  end
+
+  context 'ObscuringReferences' do
+    let!(:subject) {ObscuringReferences.new([[622, 20], [622, 23], [559, 30], [559, 40]])}
+
+    it 'Initiated properly' do
+      expect(subject).to be_an_instance_of(ObscuringReferences)
+    end
+
+    it 'Responds to proper methods' do
+      expect(subject).to respond_to(:data)
+      expect(subject).to respond_to(:diameters)
+    end
+
+    it 'Responds to diameters properly' do
+      expect(subject.diameters).to match([662, 668, 619, 639])
+    end
+
   end
 
 end
