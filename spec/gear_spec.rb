6691fcdc7c8d10c5b0507f9ec130bb34b2b87dbe
Roberto Nogueira <enogrob@gmail.com>
2018-05-13 19:57:43 -0300

    add-gear_inches-method

--- spec/gear_spec.rb
+++ spec/gear_spec.rb
@@ -3,7 +3,7 @@ require_relative '../gear'
 RSpec.describe 'POOD App' do
 
   context 'Gear' do
-    let!(:subject) {Gear.new(52, 11)}
+    let!(:subject) {Gear.new(52, 11, 0, 0)}
 
     it 'initialize properly' do
       expect(subject).to be_an_instance_of(Gear)
@@ -12,12 +12,19 @@ RSpec.describe 'POOD App' do
     it 'Responds to proper methods' do
       expect(subject).to respond_to(:chainring)
       expect(subject).to respond_to(:cog)
+      expect(subject).to respond_to(:rim)
+      expect(subject).to respond_to(:tire)
 
     end
 
     it 'Responds to ratio properly' do
-      expect(Gear.new(52,11).ratio).to be(4.7272727272727275)
-      expect(Gear.new(30,27).ratio).to be(1.1111111111111112)
+      expect(Gear.new(52,11,0,0).ratio.round(2)).to be(4.73)
+      expect(Gear.new(30,27,0,0).ratio.round(2)).to be(1.11)
+    end
+
+    it 'Responds to gear_inches properly' do
+      expect(Gear.new(52,11, 26, 1.5).gear_inches.round(2)).to be(137.09)
+      expect(Gear.new(52,11, 24, 1.25).gear_inches.round(2)).to be(125.27)
     end
   end
 
